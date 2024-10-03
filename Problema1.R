603a8007ad368618153437bbda7025a34a7efd65
elena.perez.salazar <elena.perez.salazar@auleseebe.upc.edu>
2024-10-03 11:28:31 +0200

    ...

--- Problema1.R
+++ Problema1.R
@@ -1,2 +1,17 @@
 #Codigo para problema 1
 
+#Respuesta1 = R^2 = 0.1923 #lo obtengo mirando los datos directamente
+
+#Respuesta2 = R = 0.4385202
+sqrt(0.1923)
+
+#Respuesta3 = 54.37718
+
+m <- 0.0658
+b <- 11.798
+x <- 647.1
+#estos valores los obtengo mirando los datos
+
+m*x + b
+
+#Respuesta4 = B (no se sabe)
